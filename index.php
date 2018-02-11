19e45a76b033ad740bc7f5b7155d03d130df035d
Ben Humphries <ben_humphries@hotmail.co.uk>
2018-02-11 12:23:30 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>The greatest news app ever</title>
+    <script src="https://unpkg.com/vue"></script>
+    <script src="/dist/js/app.js"></script>
+</head>
+<body>
+<div class="container" id="app">
+    <h3 class="text-center">VueNews</h3>
+</div>
+</body>
+</html>
